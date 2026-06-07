b369160a37691a164cfb30e8397ff262b0ab5df3
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-06-06 20:17:16 -0600

    First Excersice

---
+++ variables_ia.py
@@ -0,0 +1,10 @@
+nombre_modelo = "GPT-4"
+max_tokens = 2000
+cost_per_token = 50
+en_produccion = True
+
+
+print("Model: ", nombre_modelo)
+print("Tokens: ", max_tokens)
+print("Cost per token: ", cost_per_token)
+print("In Production: ", en_produccion)
