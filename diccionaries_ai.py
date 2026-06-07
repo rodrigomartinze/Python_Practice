7488434f450dab6ddb2c74d3c6a7c00858e915cc
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-06-06 20:35:13 -0600

    Diccionaries

---
+++ diccionaries_ai.py
@@ -0,0 +1,14 @@
+model_info = {
+    "name": "Fortnite",
+    "version": 10.9,
+    "accuracy": 10,
+    "active": True,
+}
+
+print(model_info["name"])
+print(model_info["accuracy"])
+
+model_info["developer"] = "Tesla"
+
+print(model_info.keys())
+print(model_info)
