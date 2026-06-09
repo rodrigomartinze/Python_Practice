9d631238990b25ddf3dfa9ff0c0d92ae5cd293e0
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-06-09 16:21:59 -0600

    diccionaries_ai2

---
+++ diccionaries/diccionaries_ai2.py
@@ -0,0 +1,10 @@
+model = {"name": "Chat-GPT", "accuracy": 0.98, "company": "OpenAI"}
+
+print(model["name"])
+print(model["Company"])
+print(model["accuracy"])
+
+model["version"] = 1.1
+
+
+print(model["version"])
