68311c0ab593c9b4d7777c83b277cbb32f7d65ee
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-06-20 20:56:12 -0600

    mean

--- numpy_ia.py
+++ numpy_ia.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 accuracy_scores = np.array([0.76, 0.12, 0.56, 0.78, 0.99])
-mean = 0
-for i in accuracy_scores:
-    mean += i
-mean = mean / len(accuracy_scores)
+# mean = 0
+# for i in accuracy_scores:
+#     mean += i
+# mean = mean / len(accuracy_scores)
+mean = accuracy_scores.mean()
 print(mean)
 
-print(accuracy_scores.max())  # → highest score
-print(accuracy_scores.min())  # → lowest score
-print(accuracy_scores > 0.85)  # → try this one and see what it prints!
+print(accuracy_scores.max())
+print(accuracy_scores.min())
+print(accuracy_scores > 0.85)
 print(sum(accuracy_scores > 0.85))
