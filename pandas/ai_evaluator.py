a7dcf1a0d75dbdce6945f90bb6318a46d7ad0eee
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-06-28 22:40:43 -0600

    Evaluator

---
+++ pandas/ai_evaluator.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+models_data = {
+    "name": ["Chat-GPT", "Gemini", "Claude", "Copilot", "Sora"],
+    "accuracy": [0.78, 0.81, 0.99, 0.92, 0.64],
+    "company": ["OpenAI", "Google", "Anthropic", "Microsoft", "Google"],
+    "version": [5, 1.8, 1.13523, 12, 3.3],
+}
