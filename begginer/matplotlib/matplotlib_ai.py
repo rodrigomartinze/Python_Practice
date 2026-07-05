274a9b5dc323b3542f8513f62e8a231a93848493
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-07-04 20:57:17 -0600

    Making a chart with pyplot about AI-Models

---
+++ begginer/matplotlib/matplotlib_ai.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+models = ["Chat-GTP", "Gemini", "Claude", "Copilot", "BlackBox"]
+acuraccy = [0.89, 0.78, 0.99, 0.91, 0.62]
+
+
+plt.bar(models, acuraccy)
+plt.title("AI-MODELS")
+plt.xlabel("Model")
+plt.ylabel("Accuracy")
+plt.show()
