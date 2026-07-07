c2852e25b36ea6ba89b3859de9a117aa8638d278
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-07-07 16:19:57 -0600

    Creating an AI models DataFrame

---
+++ AI_API/data_analyzer.py
@@ -0,0 +1,16 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+
+models_df = {
+    "model": ["Chat-GPT", "Gemini", "Claude", "Copilot", "Sora"],
+    "accuracy": [0.87, 0.79, 0.99, 0.91, 0.56],
+    "company": ["OpenAI", "Google", "Anthropic", "Microsoft", "OpenAI"],
+    "parameters_billions": [1800, 540, 137, 350, 70],
+}
+
+models_df = pd.DataFrame(models_df)
