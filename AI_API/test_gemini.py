742e1b4f56f68513dd264f63331cfae329d16249
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2026-07-27 20:57:29 -0600

    Update

--- AI_API/test_gemini.py
+++ AI_API/test_gemini.py
@@ -8,7 +8,7 @@ load_dotenv()
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",  # ← updated model name!
+    model="llama-3.3-70b-versatile", 
     messages=[{"role": "user", "content": "Say hello in one sentence!"}],
 )
 
